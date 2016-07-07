b81fff70b8dca754a952d6ac53926c1bc3fc339d
TehAsianOne <zfinalez@gmail.com>
2016-07-07 12:15:31 -0400

    Added RemoveCharacter.java

---
+++ Practice/src/ruiz/RemoveCharacter.java
@@ -0,0 +1,41 @@
+package ruiz;
+
+import java.util.Scanner;
+
+public class RemoveCharacter 
+{
+	public static void main(String[] args)
+	{
+		//User inputs a word, select what letter to remove
+
+		//User will input string
+		System.out.println("Enter a word: ");
+		Scanner scan = new Scanner(System.in);
+		String userString = scan.nextLine();
+
+		//ask what letter to be removed
+
+		System.out.print("Enter a character to remove: ");
+		char letter = scan.next().charAt(0);
+		char[] wordContainer = userString.toCharArray();
+
+
+		System.out.println(wordContainer);
+		String newWord;
+		//remove letter
+		
+		for(int x = 0; x < userString.length() - 1; x++)
+		{
+			if(wordContainer[x] == letter)
+			{
+				String firstHalf = userString.substring(0, x);
+				String secondHalf = userString.substring(x + 1, userString.length());
+				
+				newWord = firstHalf.concat(secondHalf);
+				System.out.println(newWord);
+			}
+		}
+		
+		scan.close();
+	}
+}
